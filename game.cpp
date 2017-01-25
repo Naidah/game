2b181f0ca1e3e4918e563d1c11bd4431dc098fea
Naidah <aidanwhunt@gmail.com>
2017-01-25 17:02:39 +1100

    Modulated initilisation functionality for SDL, set up colour change on the window learning to better understand SDL functionalities, added more subsections to file to better organise code

--- game.cpp
+++ game.cpp
@@ -18,12 +18,39 @@
 #include <SDL.h> // sdl library for graphics features
 #include "game.h" // contains program constants to avoid cluttering main file
 
-// Support functions
-void quitGame(SDL_Window* window) {
+/* -------------------------- STRUCTS --------------------------- */
+
+
+
+
+
+/* -------------------------- FUNCTIONS ------------------------- */
+void quitGame(SDL_Window* window) { // Terminates SDL and Frees any used memory at end of program
     SDL_DestroyWindow(window);
     SDL_Quit();
 }
 
+bool init(SDL_Window** window) { // initialize important SDL functionalities
+    bool success = true; // flag to check whether program runs successfully
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) { // Make sure SDL can initialize properly, otherwise return error code
+        printf("Error Initializing SDL./n SDL_Error %s\n", SDL_GetError());
+        success = false;
+    } else {
+        *window = SDL_CreateWindow(WINDOW_NAME, SDL_WINDOWPOS_UNDEFINED, 
+            SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT,
+             SDL_WINDOW_SHOWN);
+        if (window == NULL) {
+            printf("Error Creating Window./n SDL_Error %s\n", SDL_GetError());
+            success = false;
+        }
+    }
+    return success;
+}
+
+
+
+
+/* -------------------------- CLASSES --------------------------- */
 class Player {
     // details about the characters location
     int posx = 0;
@@ -32,36 +59,42 @@ class Player {
 
 public:
     //getters for the private variables
-    int getX(void) {return posx};
-    int getY(void) {return poxy};
-    int getAngle(void) {return angle};
+    int getX(void) {return posx;}
+    int getY(void) {return posy;}
+    int getAngle(void) {return angle;}
     void updatePosition(void);
-}
+};
 
-void Player::drawPlayer()
+void Player::updatePosition(void) {
+
+};
 
 int main(int argc, char const *argv[])
 {
+    int red = 0;
+    int blue = 0;
+    int green = 0;
+    int rounds = 0;
+
+    bool gameRunning = true; // variable to control the game loop
     SDL_Window* window = NULL;
     SDL_Surface* background = NULL;
+    init(&window);
+    background = SDL_GetWindowSurface(window);
 
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) { // Make sure SDL can initialize properly, otherwise return error code
-        printf("Error Initializing SDL./n SDL_Error %s\n", SDL_GetError());
-    } else {
-        window = SDL_CreateWindow(WINDOW_NAME, SDL_WINDOWPOS_UNDEFINED, 
-            SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT,
-             SDL_WINDOW_SHOWN);
-        if (window == NULL) {
-            printf("Error Creating Window./n SDL_Error %s\n", SDL_GetError());
-        } else {
-            background = SDL_GetWindowSurface(window);
-            SDL_FillRect(background, NULL, SDL_MapRGB(background->format,
-             0xFF, 0xFF, 0xFF));
-            SDL_UpdateWindowSurface(window);
-            SDL_Delay(5000);
-        }
+    while (rounds < 1000) {
+        SDL_FillRect(background, NULL, SDL_MapRGB(background->format, red, green, blue));
+        
+        red = (green * 2)%255;
+        green = (green + 2)%255;
+        blue = (blue + 1)%255;
+        rounds += 1;
+    
+        SDL_Delay(10);
+        SDL_UpdateWindowSurface(window);
     }
+
     quitGame(window);
 
     return EXIT_SUCCESS; // return success if the program terminates correctly
